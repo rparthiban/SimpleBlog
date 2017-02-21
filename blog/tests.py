4e56a04ffad5f634d4f7de55258bbe786cc4c94c
Parthiban <parthiban@maasjuju.maasjuju.com>
2017-02-21 15:21:23 +0530

    test case for upcoming and recent blogs, posted field verified

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,21 @@
+import datetime
+
+from django.utils import timezone
 from django.test import TestCase
 
-# Create your tests here.
+from models import Blog
+
+
+class BlogMethodTests(TestCase):
+
+    def test_was_posted_recently_with_upcoming_blog(self):
+
+        time = timezone.now() + datetime.timedelta(days=30)
+        up_blog = Blog(posted=time)
+        self.assertIs(up_blog.was_posted_recently(), False)
+
+    def test_was_posted_recently(self):
+
+        time = timezone.now()
+        b = Blog(posted=time)
+        self.assertIs(b.was_posted_recently(), True)
